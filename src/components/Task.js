96e0ad90b86d4bb5dc2cc28271006ac42d3709fa
Miroslav <mbochko99@gmail.com>
2025-01-29 11:09:54 +0200

    feat: add Task class for represent tasks

---
+++ src/components/Task.js
@@ -0,0 +1,8 @@
+export class Task {
+  constructor(taskTitle, taskDescription, taskDeadline, taskPriority) {
+    this.taskTitle = taskTitle;
+    this.taskDescription = taskDescription;
+    this.taskDeadline = taskDeadline;
+    this.taskPriority = taskPriority;
+  }
+}
