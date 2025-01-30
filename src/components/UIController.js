f7d5508c886eb3b7ea308001fd46e63ef5606ff5
Miroslav <mbochko99@gmail.com>
2025-01-30 14:54:42 +0200

    feat: added methods for rendering and initializing forms for tasks

--- src/components/UIController.js
+++ src/components/UIController.js
@@ -3,8 +3,9 @@ import { ProjectManager } from "./ProjectManager";
 export class UIController {
   constructor(projectManager) {
     this.projectManager = projectManager;
-    this.selectedProject;
+    this.selectedProject = null;
     this.projectFormInit();
+    this.taskFormInit();
   }
 
   projectFormInit() {
@@ -27,18 +28,89 @@ export class UIController {
 
   renderProjects(projects) {
     const projectContainer = document.querySelector(".project-container");
+
     projectContainer.innerHTML = "";
 
     projects.forEach((project) => {
-      const projectCard = document.createElement("div");
+      const projectCard = document.createElement("button");
       projectCard.classList.add("project-card");
       projectContainer.appendChild(projectCard);
 
       const cardTitle = document.createElement("h1");
       cardTitle.textContent = `${project.projectTitle}`;
       projectCard.appendChild(cardTitle);
+
+      projectCard.addEventListener("click", () => {
+        this.selectedProject = project;
+        this.renderTasks(this.selectedProject.getTasks());
+      });
     });
   }
 
-  renderTasks(tasks) {}
+  renderTasks(tasks) {
+    const taskContainer = document.querySelector(".task-container");
+    taskContainer.innerHTML = "";
+
+    tasks.forEach((task) => {
+      const taskCard = document.createElement("div");
+      taskCard.classList.add("task-card");
+      taskContainer.appendChild(taskCard);
+
+      const topContainer = document.createElement("div");
+      topContainer.classList.add("card-top");
+      taskCard.appendChild(topContainer);
+
+      const bottomContainer = document.createElement("div");
+      bottomContainer.classList.add("card-bottom");
+      taskCard.appendChild(bottomContainer);
+
+      const cardTitle = document.createElement("h2");
+      cardTitle.textContent = `${task.taskTitle}`;
+      topContainer.appendChild(cardTitle);
+
+      const cardDescription = document.createElement("span");
+      cardDescription.textContent = `${task.taskDescription}`;
+      cardDescription.style.color = "grey";
+      topContainer.appendChild(cardDescription);
+
+      const cardPriority = document.createElement("span");
+      cardPriority.textContent = `PRIORITY: ${task.taskPriority}`;
+      bottomContainer.appendChild(cardPriority);
+
+      const cardDeadline = document.createElement("span");
+      cardDeadline.textContent = `DEADLINE: ${task.taskDeadline}`;
+      bottomContainer.appendChild(cardDeadline);
+
+      if (task.taskPriority === "Low") {
+        taskCard.style.backgroundColor = "green";
+      }
+    });
+  }
+
+  taskFormInit() {
+    const taskForm = document.querySelector(".task-form");
+
+    taskForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const titleValue = document.querySelector("#task-title").value;
+      const descriptionValue = document.querySelector("#description").value;
+      const dateValue = document.querySelector("#date").value;
+      const priorityValue = document.querySelector("#priority").value;
+
+      if (!this.selectedProject) {
+        alert("First, select a project!");
+        return;
+      } else {
+        this.selectedProject.addTask(
+          titleValue,
+          descriptionValue,
+          dateValue,
+          priorityValue
+        );
+        this.renderTasks(this.selectedProject.getTasks());
+        taskForm.reset();
+      }
+    });
+  }
 }
